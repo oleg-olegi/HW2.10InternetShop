8ce33866bd110de293371fa2402f02ae22b710c5
oleg-olegi <ol.shink87@gmail.com>
2023-10-26 12:35:40 +0300

    Возврат сообщения что товары добавлены в корзину (множество товаров)

--- src/main/java/com/example/hw2_10internetshop/controller/ShopController.java
+++ src/main/java/com/example/hw2_10internetshop/controller/ShopController.java
@@ -5,17 +5,20 @@ import com.example.hw2_10internetshop.service.ShopServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/order")
 @AllArgsConstructor
 public class ShopController {
 
     private final ShopServiceImpl shopServiceImpl;
-
-    @GetMapping("/add")
-    public Integer addItemToCart(@RequestParam Integer itemID) {
-        shopServiceImpl.addItemToCart(itemID);
-        return itemID;
+@GetMapping("/add")
+    public String addItemToCart(@RequestParam(value = "itemID") List<Integer> itemIDs) {
+        for (Integer itemID : itemIDs) {
+            shopServiceImpl.addItemToCart(itemID);
+        }
+        return "Items added to the cart: " + itemIDs.toString();
     }
 
 
